6863183a74aac214fbad6e886c9d44e8cb78f435
g00gol <38109082+g00gol@users.noreply.github.com>
2018-06-22 16:20:56 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+//Hide Nav
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+//Smooth Scroll
+$(document).ready(function(){
+  $(".nav > a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+$(document).ready(function(){
+  $(".home_page > .home_title > a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
+
+//AOS
+$(function() {
+  AOS.init();
+});
+
+$(window).on('load', function() {
+  AOS.refresh();
+});
