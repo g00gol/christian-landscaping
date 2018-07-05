6d753f8a01d38c9f89cc66af2ba2a2f06e704e3d
g00gol <38109082+g00gol@users.noreply.github.com>
2018-07-05 12:22:44 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,24 @@ window.onscroll = function() {
 
 
 
+
+
+//Hide Side Nav
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("side-button").style.top = "0";
+  } else {
+    document.getElementById("side-button").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
+
 //Smooth Scroll
 $(document).ready(function(){
   $(".nav > a").on('click', function(event) {
@@ -40,6 +58,21 @@ $(document).ready(function(){
     }
   });
 });
+
+
+
+
+
+//Side Navigation
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+
+
 
 
 
